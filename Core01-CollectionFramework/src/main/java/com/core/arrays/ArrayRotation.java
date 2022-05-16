9dc3ccff3f7fc48ba629dbf0b0ae4941c30977bf
Nazeer <nazeersd.java@gmail.com>
2022-05-17 01:28:01 +0530

    Array Rotation updated

--- Core01-CollectionFramework/src/main/java/com/core/arrays/ArrayRotation.java
+++ Core01-CollectionFramework/src/main/java/com/core/arrays/ArrayRotation.java
@@ -3,13 +3,13 @@ package com.core.arrays;
 import java.util.Arrays;
 
 /**
- * public static void arraycopy(Object source_arr, int sourcePos, Object
- * dest_arr, int destPos, int len)
+ * public static void arraycopy(Object source_arr, int sourcePos, Object dest_arr, int destPos, int len)
  * 
- * source_arr : array to be copied from sourcePos : starting position in source
- * array from where to copy dest_arr : array to be copied in destPos : starting
- * position in destination array, where to copy in len : total no. of components
- * to be copied.
+ * source_arr : array to be copied from 
+ * sourcePos : starting position in source array from where to copy 
+ * dest_arr : array to be copied in 
+ * destPos : starting position in destination array, where to copy in 
+ * len : total no. of components to be copied.
  */
 
 public class ArrayRotation {
